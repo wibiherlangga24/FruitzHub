afbe75eff386a35e1d1725b4605066d773d07d3b
Herlangga Wibi <herlanggawibi01@gmail.com>
2023-11-04 18:36:08 +0700

    Initial Commit

---
+++ FruitzHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FruitzHub
+//
+//  Created by User on 04/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
